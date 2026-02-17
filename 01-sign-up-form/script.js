6bb9b7cf2fad21e537aa0b9d5642e85ce6405871
cylee1668 <cylee1668@gmail.com>
2026-02-17 17:27:08 +0800

    green-bg button animation

--- 01-sign-up-form/script.js
+++ 01-sign-up-form/script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
 const signUpBtn = document.querySelector(".green-bg button");
 
-signUpBtn.addEventListener("click",
-    () => {
+signUpBtn.addEventListener("click", () => {
         container.classList.toggle("change");
     }
 )
+
+// signUpBtn.addEventListener("click", () => {
+//     container.classList.toggle("change");
+//   });
