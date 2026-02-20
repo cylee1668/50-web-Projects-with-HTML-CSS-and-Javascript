4b92bb3209b68704e374888de484d7a42fece866
cylee1668 <cylee1668@gmail.com>
2026-02-21 00:21:53 +0800

    signupX loginX btn script

--- 09-Landing-Page-with-Modal-Forms/script.js
+++ 09-Landing-Page-with-Modal-Forms/script.js
@@ -3,6 +3,9 @@ const loginModal = document.querySelector(".login-form-wrapper");
 const signupBtn = document.querySelector(".signup-btn");
 const loginBtn = document.querySelector(".login-btn");
 
+const signupX = document.querySelector(".signup-x");
+const loginX = document.querySelector(".login-x");
+
 
 signupBtn.addEventListener("click", () => {
     signupModal.classList.add("display");
@@ -12,5 +15,10 @@ loginBtn.addEventListener("click", () => {
     loginModal.classList.add("display");
 })
 
+signupX.addEventListener("click", () => {
+    signupModal.classList.remove("display");
+})
 
-signupModal.classList.remove("display");
+loginX.addEventListener("click", () => {
+    loginModal.classList.remove("display");
+})
