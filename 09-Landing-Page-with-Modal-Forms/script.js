743d11927bd4480201b2f0f83684ec7ea98e3179
cylee1668 <cylee1668@gmail.com>
2026-02-21 00:08:12 +0800

    sign up login display

--- 09-Landing-Page-with-Modal-Forms/script.js
+++ 09-Landing-Page-with-Modal-Forms/script.js
@@ -1,2 +1,13 @@
 const signupModal = document.querySelector(".signup-form-wrapper");
 const loginModal = document.querySelector(".login-form-wrapper");
+const signupBtn = document.querySelector(".signup-btn");
+const loginBtn = document.querySelector(".login-btn");
+
+
+signupBtn.addEventListener("click", () => {
+    signupModal.classList.add("display");
+})
+
+loginBtn.addEventListener("click", () => {
+    loginModal.classList.add("display");
+})
