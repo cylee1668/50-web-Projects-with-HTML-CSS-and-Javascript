d88332b57a1bfe41e7596da027eb6ee4b2a16176
cylee1668 <cylee1668@gmail.com>
2026-03-10 00:55:04 +0800

    javascript changeslide

---
+++ 21-Slideshow/script.js
@@ -0,0 +1,21 @@
+let current = 1;
+
+const changeSlides = () => {
+    const slideList = document.querySelectorAll('.slide')
+
+    const slides = Array.from(slideList);
+
+    slides.forEach(slide => {
+        if(slide.classList[1].split('-')[1] * 1 ===  current) {
+            slide.style.cssText = 'visiblity: visible; opacity:1' 
+        } else {
+            slide.style.cssText = 'visiblity: visible; opacity:0' 
+        }
+    })
+};
+
+changeSlides();
+
+
+
+
