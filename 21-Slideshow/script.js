ddde937c44c29d4d99c42e8c7915090eddb36773
cylee1668 <cylee1668@gmail.com>
2026-03-10 22:32:49 +0800

    left right arrow

--- 21-Slideshow/script.js
+++ 21-Slideshow/script.js
@@ -1,21 +1,83 @@
-let current = 1;
+let current = 1,
+    playPauseBool = true,
+    interval;
 
 const changeSlides = () => {
     const slideList = document.querySelectorAll('.slide')
 
     const slides = Array.from(slideList);
 
+    if (current > slides.length) {
+        current = 1;
+    } else if(current === 0) {
+        current = slides.length;
+    }
+
     slides.forEach(slide => {
-        if(slide.classList[1].split('-')[1] * 1 ===  current) {
-            slide.style.cssText = 'visiblity: visible; opacity:1' 
+        if (slide.classList[1].split('-')[1] * 1 === current) {
+            slide.style.cssText = 'visiblity: visible; opacity:1'
         } else {
-            slide.style.cssText = 'visiblity: visible; opacity:0' 
+            slide.style.cssText = 'visiblity: visible; opacity:0'
         }
     })
 };
 
-changeSlides();
+const playPause = () => {
+    if (playPauseBool) {
+        interval = setInterval(() => {
+            current++;
+            changeSlides();
+        }, 3000);
 
+        playPauseBool = false;
+    } else {
+        clearInterval(interval);
+        playPauseBool = true;
+    }
+
+    changePlayPause();
+};
+
+const changePlayPause = () => {
+    const icon = document.querySelector
+        (".play-pause i")
+
+    const cls = icon.classList[1]
+
+    if (cls === 'fa-play') {
+        icon.classList.remove('fa-play')
+        icon.classList.add('fa-pause')
+    } else {
+        icon.classList.remove('fa-pause')
+        icon.classList.add('fa-play')
+    }
+}
+
+document.querySelector('.play-pause')
+    .addEventListener('click', () => {
+        playPause()
+    });
+
+document.querySelector(".left-arrow").addEventListener('click', () => {
+    if(!playPauseBool){
+        playPause()
+    }
+
+    current--;
+    changeSlides();
+})
+
+document.querySelector(".right-arrow").addEventListener('click', () => {
+    if(!playPauseBool){
+        playPause()
+    }
+
+    current++;
+    changeSlides();
+})
+
+changeSlides();
+playPause();
 
 
 
